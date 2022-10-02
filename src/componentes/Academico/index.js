f984e265509c1ceeb81b7dc570ab152409af9eb3
Victor Hugo <victorhugofny@gmail.com>
2022-10-01 21:39:50 -0300

    coisas novas

--- src/componentes/Academico/index.js
+++ src/componentes/Academico/index.js
@@ -1,10 +1,46 @@
 
+import './style.css'
+
 function Academico(){
     
     return(
-        <div>
-            <h1>teste</h1>
-        </div>
+
+<div className="trabalhos">
+    <h1>Trabalhos Acadêmicos</h1>
+    <p>Abaixo meus trabalhos e estudos como artigos e minicursos</p>
+
+    <div className="trabalhos"> 
+
+        <h2>ARTIGO: "SEGURANÇA DA INFORMAÇÃO NO CONTEXTO ESCOLAR"</h2> 
+        <p>Escrito por Victor Hugo de Carvalho Santana, Douglas Silva Santos, Luis Henrique de Almeida Gomes</p> 
+        <h3>Editora Realize</h3> <a> Publicado em 17 de janeiro de 2022</a>
+        <object data="https://editorarealize.com.br/editora/anais/conedu/2021/TRABALHO_EV150_MD1_SA119_ID1208_22092021174849.pdf#zoom=92" type="application/pdf">
+            <p>Seu navegador não tem um plugin pra PDF</p>
+        </object>
+        <iframe className = "artigo" src="" width="90%" height="600" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+        <a target="_blank" href="https://editorarealize.com.br/artigo/visualizar/80791"><button> <p className="btntext"> Publicação </p></button></a>
+    </div>
+    <div className="trabalhos"> 
+        <h2>MINICURSO - INTRODUÇÃO A ALGORITMO EM JAVASCRIPT</h2> 
+        <p>Ministrado por Victor Hugo, Douglas Silva, Marcos Santos</p> 
+        <a> Publicado em 26 de março de 2021</a>
+
+        <figure>
+            <div className="boxVideo">
+                <iframe width="250" height="180" src="https://www.youtube.com/embed/h5i7P6rqAhU" frameborder="0" allowfullscreen></iframe> 
+            </div>
+        </figure>
+
+        <figure>
+            <div className="boxVideo">
+                <iframe width="250" height="180" src="https://www.youtube.com/embed/HBbky2hqbOg" frameborder="0" allowfullscreen></iframe> 
+            </div>
+        </figure>
+
+
+    </div>
+
+</div>
     )
 }
 
