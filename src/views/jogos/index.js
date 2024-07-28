409b3527f7e6b9d41ff4bb33d6fef032fd98dfa8
Victor Hugo Santana <victorhugofny@gmail.com>
2024-07-28 20:30:25 -0300

    Update index.js

--- src/views/jogos/index.js
+++ src/views/jogos/index.js
@@ -40,6 +40,10 @@ function Home(){
                 </div>
 
                 <div className="boxVideo">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/11QDRHl3MCQ?si=PVHT2G3JKSMUWYS2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+                
+                <div className="boxVideo">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/YyOWEPDoTd8?si=Ww_IuznwKlaDlTJs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                
                 </div>
                 
